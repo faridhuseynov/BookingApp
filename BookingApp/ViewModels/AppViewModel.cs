95ac59d8a8dc98c57a5908f74ba392b7235124a8
Farid Huseynov <F.H189@hotmail.com>
2019-01-02 11:48:29 +0400

    Services created

---
+++ BookingApp/ViewModels/AppViewModel.cs
@@ -0,0 +1,16 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.ViewModels
+{
+    class AppViewModel : ViewModelBase
+    {
+        private ViewModelBase currentPage;
+        public ViewModelBase CurrentPage { get => currentPage; set => Set(ref currentPage, value); }
+
+    }
+}
